97d8e17c674ed2d98ebc25f9ee8d4e9e4d1469e1
Your Name <ronaldov_zelada@hotmail.com>
2017-04-06 00:35:54 -0500

    segundo hola

---
+++ testAplication/testAplication/Radio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testAplication
+{
+    public class Radio
+    {
+        public string Nombre { get; set; }
+
+    }
+}
